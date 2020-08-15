d9ed16239df99baa83e728230115e605495a37cb
Bria White <bria.awhite@gmail.com>
2020-08-15 19:52:19 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,14 +4,5 @@ require_relative './fancy_dance.rb'
 
 class Kid 
   
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-  
-  extend MetaDancing
   
 end 
